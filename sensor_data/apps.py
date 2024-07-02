7a06becafbd863cfeaf0e97cf175e9c3450fd0e2
Amin Mortezaie <mohammadmortezaie@gmail.com>
2024-07-02 20:28:47 +0330

    Feat: add mqtt listener to ready func.

--- sensor_data/apps.py
+++ sensor_data/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class SensorDataConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sensor_data'
+
+    def ready(self):
+        from sensor_data.mqtt.mqtt_client import start_mqtt
+        start_mqtt()
