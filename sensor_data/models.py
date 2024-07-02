80c7dfb5e7c7c7aacbd330cb261aaabf2837ff2d
Amin Mortezaie <mohammadmortezaie@gmail.com>
2024-07-02 20:27:38 +0330

    Fix: change fields.

--- sensor_data/models.py
+++ sensor_data/models.py
@@ -4,8 +4,7 @@ class SensorData(models.Model):
     hub_id = models.CharField(max_length=100)
     temperature = models.FloatField()
     humidity = models.FloatField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.hub_id} - {self.created_at}"
+        return f"{self.hub_id} - {self.timestamp}"
